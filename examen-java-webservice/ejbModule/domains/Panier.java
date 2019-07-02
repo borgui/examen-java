c0dbaa6d450789ec1a2b9c281873a9d9ae703460
Bioski1 <bioski10@gmail.com>
2019-07-02 13:13:19 +0200

    Correction

---
+++ examen-java-webservice/ejbModule/domains/Panier.java
@@ -0,0 +1,78 @@
+package domains;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "Paniers" )
+public class Panier {
+
+    @Id
+    @Column( name = "ID" )
+    Integer id;
+
+    @Column( name = "QUANTITE" )
+    Integer quantite;
+
+    @Column( name = "idProduit" )
+    Integer idProduit;
+
+    @Column( name = "idUsers" )
+    Integer idUser;
+
+    @Column( name = "idVendeur" )
+    Integer idVendeur;
+
+    @Column( name = "idFDP" )
+    Integer idFDP;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
+    }
+
+    public Integer getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite( Integer quantite ) {
+        this.quantite = quantite;
+    }
+
+    public Integer getIdProduit() {
+        return idUser;
+    }
+
+    public void setIdProduit( Integer idProduit ) {
+        this.idProduit = idProduit;
+    }
+
+    public Integer getIdUsers() {
+        return idUser;
+    }
+
+    public void setIdUser( Integer idUser ) {
+        this.idUser = idUser;
+    }
+
+    public Integer getIdVendeur() {
+        return idVendeur;
+    }
+
+    public void setIdVendeur( Integer idVendeur ) {
+        this.idVendeur = idVendeur;
+    }
+
+    public Integer getIdFDP() {
+        return idFDP;
+    }
+
+    public void setIdFDP( Integer idFDP ) {
+        this.idFDP = idFDP;
+    }
+}
---
+++ examen-java-webservice/ejbModule/domains/Produits.java
@@ -0,0 +1,79 @@
+package domains;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "Profils" )
+public class Produits {
+
+    @Id
+    @Column( name = "ID" )
+    Integer id;
+
+    @Column( name = "NOM" )
+    String  nom;
+
+    @Column( name = "PRIX" )
+    Integer prix;
+
+    @Column( name = "STOCK" )
+    Integer stock;
+
+    @Column( name = "idCategorie" )
+    Integer idCategorie;
+
+    @Column( name = "idEntrepot" )
+    Integer idEntrepot;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom( String nom ) {
+        this.nom = nom;
+    }
+
+    public Integer getPrix() {
+        return prix;
+    }
+
+    public void setPrix( Integer prix ) {
+        this.prix = prix;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock( Integer stock ) {
+        this.stock = stock;
+    }
+
+    public Integer getIdCategorie() {
+        return idCategorie;
+    }
+
+    public void setIdCategorie( Integer idCategorie ) {
+        this.idCategorie = idCategorie;
+    }
+
+    public Integer getIdEntrepot() {
+        return idEntrepot;
+    }
+
+    public void setIdEntrepot( Integer idEntrepot ) {
+        this.idEntrepot = idEntrepot;
+    }
+
+}
