4e11b6144102647f579ec40cba0f469da68661e1
guillaume <guillaume.bordanave@gmail.com>
2019-07-02 16:32:22 +0200

    produit

---
+++ examen-java-webapp/src/main/java/webservice/Categorie.java
@@ -0,0 +1,39 @@
+
+package webservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour categorie produit complex type.
+ * 
+ * <p>Le fragment de sch�ma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Categorie", propOrder = {
+    "id",
+    "libelle",
+})
+public class Categorie {
+	
+    protected Integer id;
+    protected String libelle;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+   
+}
